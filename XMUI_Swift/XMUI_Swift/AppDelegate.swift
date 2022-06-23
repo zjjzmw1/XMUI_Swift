5ed76ae8b7f1cd6cc6a1401a94ca61bf26616c9f
ext.zhangmingwei1 <ext.zhangmingwei1@jd.com>
2022-06-23 11:08:58 +0800

    初始化空项目添加空首页

---
+++ XMUI_Swift/XMUI_Swift/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  XMUI_Swift
+//
+//  Created by ext.zhangmingwei1 on 2022/6/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        self.window = UIWindow.init(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height))
+        self.window?.backgroundColor = UIColor.white
+        self.window?.makeKeyAndVisible()
+        let vc = ViewController.init()
+        let navi = UINavigationController.init(rootViewController: vc)
+        self.window?.rootViewController = navi
+
+        return true
+    }
+
+}
+
---
+++ XMUI_Swift/XMUI_Swift/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  XMUI_Swift
+//
+//  Created by ext.zhangmingwei1 on 2022/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.lightGray
+        self.navigationItem.title = "首页"
+
+    }
+
+
+}
+
