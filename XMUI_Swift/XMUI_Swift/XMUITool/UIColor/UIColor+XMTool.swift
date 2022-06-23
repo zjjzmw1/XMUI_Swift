dfbb3fb7187441251a1c5d0c495890cb2f897b25
ext.zhangmingwei1 <ext.zhangmingwei1@jd.com>
2022-06-23 14:29:11 +0800

    UIColor+XMTool

---
+++ XMUI_Swift/XMUI_Swift/XMUITool/UIColor/UIColor+XMTool.swift
@@ -0,0 +1,25 @@
+//
+//  UIColor+XMTool.swift
+//  XMUI_Swift
+//
+//  Created by ext.zhangmingwei1 on 2022/6/23.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    
+    /// 根据十六进制，获取 UIColor
+    /// - Parameters:
+    ///   - value: 例如：0x333333
+    ///   - alphe: 默认是不透明 1.0
+    /// - Returns: UIColor
+    class func colorWithHex(value: __uint32_t, alphe: CGFloat = 1.0) -> UIColor {
+        return UIColor.init(red: CGFloat((value & 0xFF0000) >> 16) / 255.0,
+                            green: CGFloat((value & 0xFF00) >> 8) / 255.0,
+                            blue: CGFloat((value & 0xFF)) / 255.0,
+                            alpha: alphe);
+    }
+}
